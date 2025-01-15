30b49c17a2c4f40e0c59a9befa2c90f8ea2e8574
Ronnie-Leon76 <ronleon76@gmail.com>
2025-01-15 02:18:23 +0000

    Feat: Retrain runpod

--- scripts/eval_script.sh
+++ scripts/eval_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python eval.py --data_dir ../data/audio_and_txt_files/ --folds_file ../data/patient_list_foldwise.txt --batch_size 64 --num_worker 4 --test_fold 4 --checkpoint models/ckpt_best.pkl --steth_id -1
+python eval.py --data_dir /workspace/Intelligent_Digital_Stethoscope_for_Automated_Lung_Sound_Analysis/data/audio_and_txt_files/ --folds_file /workspace/Intelligent_Digital_Stethoscope_for_Automated_Lung_Sound_Analysis/data/patient_list_foldwise.txt --batch_size 64 --num_worker 4 --test_fold 4 --checkpoint models/ckpt_best.pkl --steth_id -1
--- scripts/train_script_run.sh
+++ scripts/train_script_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python train.py --data_dir ../data/audio_and_txt_files/ --folds_file ../data/patient_list_foldwise.txt --model_path models_out --lr 1e-3 --batch_size 64 --num_worker 4 --start_epochs 0 --epochs 200 --test_fold 4
+python train.py --data_dir /workspace/Intelligent_Digital_Stethoscope_for_Automated_Lung_Sound_Analysis/data/audio_and_txt_files/ --folds_file /workspace/Intelligent_Digital_Stethoscope_for_Automated_Lung_Sound_Analysis/data/patient_list_foldwise.txt --model_path models_out --lr 1e-3 --batch_size 64 --num_worker 4 --start_epochs 0 --epochs 200 --test_fold 4
